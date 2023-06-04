a0560eade8e9d85df5e2afd1533cbb3f4cb69367
abelwei <383887138@qq.com>
2023-06-04 11:21:46 +0800

    写测试单元

--- tests/t1/main.go
+++ tests/t1/main.go
@@ -1,7 +1,18 @@
 package main
 
-func main() {
-	//_, dir := draw_image.NewLoadEnv().GetGenerateDir()
-	//类别入口
+import (
+	"github.com/abelwei/mock_image/draw_image"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	_, dir := draw_image.NewLoadEnv().GetGenerateDir()
+	drawFormDsl := `rect,w=500,h=200,color=ff0000`
+	filePath := dir + "/0t1.png"
+	err := draw_image.NewDrawPattern().Parse(drawFormDsl).SaveDisk(filePath)
+	if err != nil {
+		logrus.Errorf("dsl:%s, path:%s", drawFormDsl, filePath)
+		return
+	}
+	logrus.Infof("ok: %s", filePath)
 }
